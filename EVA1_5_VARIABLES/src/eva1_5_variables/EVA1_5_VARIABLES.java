affd7b73f2a94e151d82d80df0092db2d7900526
JorgeAlcantar <L25550707@chihuahua2.tecnm.mx>
2025-08-27 17:53:07 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author JORGE
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARACION DE VARIABLES
+        //Crear las variables para poder usarlas 
+        //TIPO DE DATO LUEGO EL IDENTIFICADOR
+        String nombre;
+        byte edad;
+        //INICIALIZACION
+        //USAMOS EL OPERADOR = --- OPERADOR DE ASIGNACION
+        //PONE "Ruben Hernandez" dentro de la variable nombre
+        nombre = "Ruben Hernandez";
+        edad = 43;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
