7a78e5471057a40fa967bd16ae64760038c1c1a3
JorgeAlcantar <L25550707@chihuahua2.tecnm.mx>
2025-09-01 17:53:00 -0600

    Add files via upload

--- EVA1_8_TEMPERATURAS/src/eva1_8_temperaturas/EVA1_8_TEMPERATURAS.java
+++ EVA1_8_TEMPERATURAS/src/eva1_8_temperaturas/EVA1_8_TEMPERATURAS.java
@@ -14,13 +14,36 @@ public class EVA1_8_TEMPERATURAS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double Cel, Fah;
+        //VARIABLES
+        double Cel, Fah, Kel;
         //INICIALIZACION
-        Cel = 24;
-        Fah = Cel * 1.8 + 32.0;
+        Fah = 100;
+        Cel = (5 * (Fah - 32.0)) / 9.0;
+        Fah = ((9.0 * Cel) /5.0) + 32.0;
+        Kel = Cel + 273.15;
         //IMPRIMIR
-        System.out.println("La escala de temperatura de grados Celsius de 34 grados a Fahrenheit es: ");
+        System.out.println("Las escalas de temperatura de Fahrenheit a celsius, de celsius a fahrenheit y de celsius a kelvin es: ");
+        System.out.println(Cel);
         System.out.println(Fah);
+        System.out.println(Kel);
+        //INICIALIZACION
+        Fah = 100;
+        Cel = (5 * (Fah - 32.0)) / 9.0;
+        //IMPRIMIR
+        System.out.print("En la operacion de Fahrenheit a Celsius es: ");
+        System.out.println(Cel);
+        //INICIALIZACIÓN
+        Cel = 37.77777777777778;
+        Fah = ((9.0 * Cel) / 5.0) +32.0;
+        //IMPRIMIR
+        System.out.print("En la operacion de Celsius a Fahrenheit es: ");
+        System.out.println(Fah);
+        //INICIALIZACION
+        Cel = 37.77777777777778;
+        Kel = Cel + 273.15;
+        System.out.print("En la operacion de Celsius a Kelvin es: ");
+        System.out.println(Kel);
+        
     }
     
 }
